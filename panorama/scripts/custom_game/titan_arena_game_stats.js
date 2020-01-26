8405cd7621e0672d1bbc6009026b05f6f8232c4c
Unknown <superdotaplaya@gmail.com>
2020-01-26 00:00:17 -0500

    some more panorama stuff

--- panorama/scripts/custom_game/titan_arena_game_stats.js
+++ panorama/scripts/custom_game/titan_arena_game_stats.js
@@ -11,16 +11,16 @@ for (i = 0; i < 10; i++) {
   player_gpm[i] = Players.GetGoldPerMin(i)
   player_name[i] = Players.GetPlayerName(i)
 }
-$("#gpm1").text =  player_name[0] + " " + player_gpm[0] + " GPM"
-$("#gpm2").text = player_name[1] + " " + player_gpm[1] + " GPM"
-$("#gpm3").text = player_name[2] + " " + player_gpm[2] + " GPM"
-$("#gpm4").text = player_name[3] + " " + player_gpm[3] + " GPM"
-$("#gpm5").text = player_name[4] + " " + player_gpm[4] + " GPM"
-$("#gpm6").text = player_name[5] + " " + player_gpm[5] + " GPM"
-$("#gpm7").text = player_name[6] + " " + player_gpm[6] + " GPM"
-$("#gpm8").text = player_name[7] + " " + player_gpm[7] + " GPM"
-$("#gpm9").text = player_name[8] + " " + player_gpm[8] + " GPM"
-$("#gpm10").text = player_name[9] + " " + player_gpm[9] + " GPM"
+$("#gpm1").text =  player_name[0] + " " + Math.trunc(player_gpm[0]) + " GPM"
+$("#gpm2").text = player_name[1] + " " + Math.trunc(player_gpm[1]) + " GPM"
+$("#gpm3").text = player_name[2] + " " + Math.trunc(player_gpm[2]) + " GPM"
+$("#gpm4").text = player_name[3] + " " + Math.trunc(player_gpm[3]) + " GPM"
+$("#gpm5").text = player_name[4] + " " + Math.trunc(player_gpm[4]) + " GPM"
+$("#gpm6").text = player_name[5] + " " + Math.trunc(player_gpm[5]) + " GPM"
+$("#gpm7").text = player_name[6] + " " + Math.trunc(player_gpm[6]) + " GPM"
+$("#gpm8").text = player_name[7] + " " + Math.trunc(player_gpm[7]) + " GPM"
+$("#gpm9").text = player_name[8] + " " + Math.trunc(player_gpm[8]) + " GPM"
+$("#gpm10").text = player_name[9] + " " + Math.trunc(player_gpm[9]) + " GPM"
 
 }
 
