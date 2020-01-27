cd8b1aadfc0066551e43c4f446ff4fda739e974f
Unknown <superdotaplaya@gmail.com>
2020-01-27 01:09:41 -0500

    Duel timer now works! needs styling still

--- panorama/scripts/custom_game/titan_arena_game_stats.js
+++ panorama/scripts/custom_game/titan_arena_game_stats.js
@@ -4,8 +4,17 @@ var dire_gpm = 0
 var player_gpm = []
 var player_name = []
 var player_id;
+var game_time;
+var time_until_duel;
+var duel_start_time;
+var current_duel;
+var time_trunc;
+var minutes;
+var seconds;
+var duel_start;
+
 // STARTS and Resets the loop if any
-function OnFoo( data ) {
+function OnEntityKilled( data ) {
 var i;
 for (i = 0; i < 10; i++) {
   player_gpm[i] = Players.GetGoldPerMin(i)
@@ -24,6 +33,39 @@ $("#gpm10").text = player_name[9] + " " + Math.trunc(player_gpm[9]) + " GPM"
 
 }
 
-    var handle = GameEvents.Subscribe( "entity_killed", OnFoo );
+function startTimer(duel_start,current_duel)
+{
+	game_time = Game.GetDOTATime( false, false )
+	time_trunc = Math.trunc(game_time)
+	minutes = (duel_start - game_time)/60
+	if (current_duel >= 1)
+	{
+	seconds = 60 - (game_time % 60);
+	} else {
+	seconds = ((minutes*60)-(duel_start-60))
+	}
+	if (game_time < duel_start)
+	{
+		$("#duel-countdown").text = "Duel starts in " + Math.trunc(minutes) + ":" + Math.trunc(seconds)
+		$.Schedule(.2, function(){startTimer(duel_start,current_duel) })
+	} 
+	if (game_time > duel_start -.5 || game_time >= duel_start + .5)
+	{
+		current_duel += 1
+		duel_start = (current_duel*420) + 60
+		$.Schedule(.2, function(){startTimer(duel_start,current_duel) })
+	}
+
+}
+function OnGameRulesStateChange ( Data )
+{
+$("#duel-countdown").text = "Duel starts in 1 minute"
+time_until_duel = 60
+duel_start_time = 60
+current_duel = 0
+startTimer(60,0)
+}
+    var handle = GameEvents.Subscribe( "entity_killed", OnEntityKilled );
+    var handle1 = GameEvents.Subscribe( "game_rules_state_change", OnGameRulesStateChange	   );
 
 
