b1d8ca3ecc18c50c04f32c91fa8b579281e55ec7
Unknown <superdotaplaya@gmail.com>
2020-01-27 18:48:17 -0500

    Added base for voting, (votes link to lua but do not do anything yet)

---
+++ panorama/scripts/custom_game/voting.js
@@ -0,0 +1,12 @@
+var has_voted = {}
+var player = Players.GetLocalPlayer()
+function Vote(voted_number)
+{
+if (has_voted[player] != true)
+{
+$.Msg("user has voted for " + voted_number + " kills")
+has_voted[player] = true
+GameEvents.SendCustomGameEventToServer( "player_voted", {vote: voted_number} )
+}
+}
+
