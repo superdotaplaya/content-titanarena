f23365a9840c26256fe6506f6aba75cdfdf49990
Unknown <superdotaplaya@gmail.com>
2020-01-27 21:56:53 -0500

    Fixed error with people voting too early

--- panorama/scripts/custom_game/voting.js
+++ panorama/scripts/custom_game/voting.js
@@ -4,9 +4,12 @@ function Vote(voted_number)
 {
 if (has_voted[player] != true)
 {
+	if (Game.GameStateIs(2))
+{
 $.Msg("user $#" + player + " has voted for " + voted_number + " kills")
 has_voted[player] = true
 GameEvents.SendCustomGameEventToServer( "player_voted", {vote: voted_number} )
+}
 }
 }
 
