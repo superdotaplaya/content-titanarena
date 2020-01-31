859c8564458a84cfbdb4e322c376843c9ea6e3cb
Unknown <superdotaplaya@gmail.com>
2020-01-30 21:04:32 -0500

    shop is linked to db, particles cant be ought but are checked to see if they are owned

--- panorama/scripts/custom_game/custom_shop.js
+++ panorama/scripts/custom_game/custom_shop.js
@@ -7,17 +7,28 @@ function loadShop()
 		$("#shop-title").text = player_name + "'s Customization Shop"
 
 
+
 }
 
 function PlayerSelectDevParticle()
 {
+	player = Game.GetLocalPlayerInfo().player_id
 $.Msg("Player selected dev particle in shop!")
-
+GameEvents.SendCustomGameEventToServer( "player_purchase_custom_item", {item: "dev", price: 200, player_id: player} )
 
 }
 
 function PlayerSelectBetaTester()
 {
+	player = Game.GetLocalPlayerInfo().player_id
 $.Msg("Player selected beta tester particle in shop!")
+GameEvents.SendCustomGameEventToServer( "player_purchase_custom_item", {item: "beta_tester", price: 200, player_id: player} )
+}
+
+function LoadCurrency(Data)
+{
+$("#player-currency").text = Data.player_currency
 
 }
+
+var handle = GameEvents.Subscribe( "player_currency_loaded", LoadCurrency );
